b0c4b85850a340fbde24861266a03b9e29d1c191
Ericg8 <145555147+Ericg8@users.noreply.github.com>
2023-11-30 10:36:29 +0100

    Add files via upload

---
+++ e3.py
@@ -0,0 +1,12 @@
+numeros = int(input("Introdueix un nombre: "))
+suma_parells = 0
+suma_senars = 0
+
+for num in range(numeros):
+    if num % 2 == 0:
+        suma_parells += num
+    else:
+        suma_senars += num
+
+print("La suma de parells és:", suma_parells)
+print("La suma de senars és:", suma_senars)
