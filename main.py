0d53d6c413292c7fe02cbad80a8b545604af6cf1
izan.fernandez.7e7 <izan.fernandez.7e7@itb.cat>
2023-11-30 11:31:32 +0100

    si

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-alcada_triangle = int(input("Alçada del triangle (entre 2 i 9): "))
+alcada_triangle = int(input("Alçada del triangle (entre 2 i 9):"))
 
 if 2 <= alcada_triangle <= 9:
-    for i in range(1, alcada_triangle +1):
+    print(("1"))
+    for i in range(2,alcada_triangle +1):
         if i == alcada_triangle:
-            print((str(i) + '') * (2 * i - 1))
+            print((str(i) + ' ') * ((i)))
         else:
-            print(str(i) + ' ' * (2 * (i - 1)) + str(i))
+            print(str(i) + ' ' * ((2*i -2)) + str(i))
 else:
     print("L'alçada ha de ser entre 2 i 9, ambdós inclosos.")
